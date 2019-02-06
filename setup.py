a0d05305f314654e878fcffd72c59d5aa85cee18
Sangwon Seo <raphaelseo@gmail.com>
2019-02-06 13:10:37 -0800

    fix code

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
   name='kgbase-client',
   version='0.1.0',
   description='A kgbase client.',
   url='https://github.com/thinknum/kgbase-client',
-  packages=find_packages()
+  packages=['kgbase_client']
 )
