6617d4f9a74c902832d96262eb790c1379d207b1
Sangwon Seo <raphaelseo@gmail.com>
2020-07-23 17:26:34 -0400

    upgrade to 0.19

--- kgbase/Query.py
+++ kgbase/Query.py
@@ -14,7 +14,7 @@ class Query(object):
     HEADERS = {
         "Accept": "application/json",
         "Content-Type": "application/json",
-        "User-Agent": "Python API 0.18 / {local_version}".format(local_version=platform.python_version())
+        "User-Agent": "Python API 0.19 / {local_version}".format(local_version=platform.python_version())
     }
 
     def __init__(self, proxies={}, verify=True):
