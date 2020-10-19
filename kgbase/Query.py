2b0057dcb7e6bb06d2adcd771663eca046e75c81
Sangwon Seo <raphaelseo@gmail.com>
2020-10-19 11:04:23 -0400

    update bulk update

--- kgbase/Query.py
+++ kgbase/Query.py
@@ -15,7 +15,7 @@ class Query(object):
     HEADERS = {
         "Accept": "application/json",
         "Content-Type": "application/json",
-        "User-Agent": "Python API 0.25 / {local_version}".format(local_version=platform.python_version())
+        "User-Agent": "Python API 0.26 / {local_version}".format(local_version=platform.python_version())
     }
 
     def __init__(self, proxies={}, verify=True):
