be74876ffb7d240ef1cceebd6883bcd155a61f5f
Sangwon Seo <raphaelseo@gmail.com>
2021-02-11 15:02:51 -0500

    remove addresses

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setuptools.setup(
     name='kgbase',
     packages=['kgbase'],
     include_package_data=True,
-    version='0.29',
+    version='0.30',
     license='MIT',
     long_description=long_description,
     long_description_content_type='text/markdown',
