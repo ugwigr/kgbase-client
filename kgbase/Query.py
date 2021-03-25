3058ef374baa9433213ac1246d55e2c3ee7bd20d
Vojtech Rinik <vojto@rinik.net>
2021-03-25 16:05:22 +0100

    Bumped version to 0.32

--- kgbase/Query.py
+++ kgbase/Query.py
@@ -15,7 +15,7 @@ class Query(object):
     HEADERS = {
         "Accept": "application/json",
         "Content-Type": "application/json",
-        "User-Agent": "Python API 0.31 / {local_version}".format(local_version=platform.python_version())
+        "User-Agent": "Python API 0.32 / {local_version}".format(local_version=platform.python_version())
     }
 
     def __init__(self, proxies={}, verify=True):
