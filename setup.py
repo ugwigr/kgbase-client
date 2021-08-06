e5b12f6a2fc03317f43c1ead971c621192cb3471
Lukáš Prokein <lukas.prokein@thinknum.com>
2021-08-06 15:07:06 +0200

    Updated version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setuptools.setup(
     name='kgbase',
     packages=['kgbase'],
     include_package_data=True,
-    version='0.32',
+    version='0.34',
     license='MIT',
     long_description=long_description,
     long_description_content_type='text/markdown',
