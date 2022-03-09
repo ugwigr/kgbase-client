7175be4943385bbd09ec29392fa026aceb62ce62
Sangwon Seo <sangwonseo@ip-10-13-37-157.us-west-2.compute.internal>
2022-03-09 14:34:58 -0500

    add link edge

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setuptools.setup(
     name='kgbase',
     packages=['kgbase'],
     include_package_data=True,
-    version='0.35',
+    version='0.36',
     license='MIT',
     long_description=long_description,
     long_description_content_type='text/markdown',
